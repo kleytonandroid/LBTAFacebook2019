593278855fa975ed505fcf6bb7251dc933189b5a
Kleyton <kleytonandroid@gmail.com>
2020-03-06 13:11:19 -0300

    Architecture of Post of app like Facebook 2019 using viewcode and SwiftUI

---
+++ FB2019_lbta/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FB2019_lbta
+//
+//  Created by Kleyton Santos on 06/03/20.
+//  Copyright © 2020 Kleyton Santos. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, 12311!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ FB2019_lbta/MainController.swift
@@ -0,0 +1,62 @@
+//
+//  MainController.swift
+//  FB2019_lbta
+//
+//  Created by Kleyton Santos on 06/03/20.
+//  Copyright © 2020 Kleyton Santos. All rights reserved.
+//
+
+import UIKit
+import LBTATools
+
+class PostCell: LBTAListCell<String> {
+    
+    let imageView = UIImageView(backgroundColor: .red)
+    let nameLabel = UILabel(text: "Name Label")
+    let dateLabel = UILabel(text: "Friday at 11:11AM")
+    let postTextLabel = UILabel(text: "Here is my post text")
+    let imageViewGrid = UIView(backgroundColor: .yellow)
+    
+    override func setupViews() {
+        backgroundColor = .white
+        
+        stack(hstack(imageView.withHeight(40).withWidth(40),
+                     stack(nameLabel,dateLabel),
+            spacing: 8).padLeft(12).padRight(12).padTop(12),
+              postTextLabel,
+              imageViewGrid, spacing: 8)
+    }
+}
+
+class MainController: LBTAListController<PostCell, String>, UICollectionViewDelegateFlowLayout {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.backgroundColor = .init(white: 0.9, alpha: 1)
+        
+        self.items = ["hello", "World", "1", "2"]
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return .init(width: view.frame.width, height: 250)
+    }
+    
+}
+
+import SwiftUI
+struct MainPreview: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) -> UIViewController {
+           return MainController()
+        }
+        
+        func updateUIViewController(_ uiViewController: MainPreview.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<MainPreview.ContainerView>) {
+            
+        }
+    }
+}
